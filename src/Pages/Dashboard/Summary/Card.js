05dc82f0320ae9ac05ddb99c1eee473438b6fc6f
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2022-01-22 00:22:43 +0600

    Sammary

---
+++ src/Pages/Dashboard/Summary/Card.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Grid, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { CountUp } from "use-count-up";
+import HourglassBottomIcon from "@mui/icons-material/HourglassBottom";
+import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
+import DriveEtaIcon from "@mui/icons-material/DriveEta";
+import ArchitectureIcon from "@mui/icons-material/Architecture";
+
+const Card = ({ bookings, ChauffeurServices, FlatRate, Hourly, Distance }) => {
+	return (
+		<div>
+			<Grid container spacing={2}>
+				<Grid item md={3} xs={12} sm={6}>
+					<Paper elevation={3}>
+						<Box display='flex' justifyContent='space-between'>
+							<Box
+								className='bgColor'
+								sx={{ p: 3, color: "white" }}
+								display='flex'
+								alignItems='center'>
+								<ArchitectureIcon sx={{ fontSize: 50 }} />
+							</Box>
+							<Box sx={{ textAlign: "right", m: 3, color: "black" }}>
+								<Typography variant='h5' component='div'>
+									Distance
+								</Typography>
+								<Typography variant='h5' component='div'>
+									<CountUp isCounting end={Distance} duration={3} />
+								</Typography>
+							</Box>
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid item md={3} xs={12} sm={6}>
+					<Paper elevation={3}>
+						<Box display='flex' justifyContent='space-between'>
+							<Box
+								className='bgColor'
+								sx={{ p: 3, color: "white" }}
+								display='flex'
+								alignItems='center'>
+								<HourglassBottomIcon sx={{ fontSize: 50 }} />
+							</Box>
+							<Box sx={{ textAlign: "right", m: 3, color: "black" }}>
+								<Typography variant='h5' component='div'>
+									Hourly
+								</Typography>
+								<Typography variant='h5' component='div'>
+									<CountUp isCounting end={Hourly} duration={3} />
+								</Typography>
+							</Box>
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid item md={3} xs={12} sm={6}>
+					<Paper elevation={3}>
+						<Box display='flex' justifyContent='space-between'>
+							<Box
+								className='bgColor'
+								sx={{ p: 3, color: "white" }}
+								display='flex'
+								alignItems='center'>
+								<TrendingFlatIcon sx={{ fontSize: 50 }} />
+							</Box>
+							<Box sx={{ textAlign: "right", m: 3, color: "black" }}>
+								<Typography variant='h5' component='div'>
+									Flat Rate
+								</Typography>
+								<Typography variant='h5' component='div'>
+									<CountUp isCounting end={FlatRate} duration={3} />
+								</Typography>
+							</Box>
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid item md={3} xs={12} sm={6}>
+					<Paper elevation={3}>
+						<Box display='flex' justifyContent='space-between'>
+							<Box
+								className='bgColor'
+								sx={{ p: 3, color: "white" }}
+								display='flex'
+								alignItems='center'>
+								<DriveEtaIcon sx={{ fontSize: 50 }} />
+							</Box>
+							<Box sx={{ textAlign: "right", m: 3, color: "black" }}>
+								<Typography variant='h5' component='div'>
+									Chauffeur
+								</Typography>
+								<Typography variant='h5' component='div'>
+									<CountUp isCounting end={ChauffeurServices} duration={3} />
+								</Typography>
+							</Box>
+						</Box>
+					</Paper>
+				</Grid>
+			</Grid>
+		</div>
+	);
+};
+
+export default Card;
--- src/Pages/Dashboard/Summary/Summary.js
+++ src/Pages/Dashboard/Summary/Summary.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import { Container, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Calander from "./Calander";
+import Card from "./Card";
+import Chart from "./Chart";
 
 const Summary = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	const [bookings, setBookings] = useState([]);
+	useEffect(() => {
+		fetch(`https://fierce-reef-90342.herokuapp.com/bookings`)
+			.then((res) => res.json())
+			.then((data) => {
+				setBookings(data.reverse());
+			});
+	}, []);
+	const Distance = bookings.filter(
+		(booking) => booking.rideType === "Distance",
+	).length;
+	const Hourly = bookings.filter(
+		(booking) => booking.rideType === "Hourly",
+	).length;
+	const FlatRate = bookings.filter(
+		(booking) => booking.rideType === "Flat Rate",
+	).length;
+	const ChauffeurServices = bookings.filter(
+		(booking) => booking.rideType === "Chauffeur Services",
+	).length;
+
+	return (
+		<Container>
+			<Grid container spacing={2} alignItems='center' sx={{ my: 1.5 }}>
+				<Grid item md={12} xs={12}>
+					<Card
+						bookings={bookings}
+						Distance={Distance}
+						Hourly={Hourly}
+						FlatRate={FlatRate}
+						ChauffeurServices={ChauffeurServices}
+					/>
+				</Grid>
+				<Grid item md={6} xs={12}>
+					<Calander />
+				</Grid>
+				<Grid item md={6} xs={12}>
+					<Chart
+						bookings={bookings}
+						Distance={Distance}
+						Hourly={Hourly}
+						FlatRate={FlatRate}
+						ChauffeurServices={ChauffeurServices}
+					/>
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default Summary;
